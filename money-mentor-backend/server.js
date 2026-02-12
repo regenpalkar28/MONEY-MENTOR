8d0eeeb80515ca7199925bd725e9f892e031ac60
regenpalkar28 <regenrahuleenapalkar@gmail.com>
2026-02-12 21:42:19 +0530

    fixed quizRouter error

--- money-mentor-backend/server.js
+++ money-mentor-backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/api/quizllm', quizRouter); 
+app.use('/api/quiz', quizRouter); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
