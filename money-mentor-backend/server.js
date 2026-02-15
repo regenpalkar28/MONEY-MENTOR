beb6a073cb70cff9831ca48deec75a3dc9698a7b
regenpalkar28 <regenrahuleenapalkar@gmail.com>
2026-02-15 18:32:59 +0530

    changed package versions

--- money-mentor-backend/server.js
+++ money-mentor-backend/server.js
@@ -24,4 +24,7 @@ app.use('/api/quiz', quizRouter);
 app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+export default app;
