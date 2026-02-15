b389eb0690f7c67fa9408b9ba6bb4842666b4d67
regenpalkar28 <regenrahuleenapalkar@gmail.com>
2026-02-15 15:42:22 +0530

    fixed login - signup functionality

--- money-mentor-backend/routes/auth.js
+++ money-mentor-backend/routes/auth.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 const router = express.Router();
 
@@ -44,4 +44,4 @@ router.post("/login", async (req, res) => {
   }
 });
 
-module.exports = router;
+export default router;
--- money-mentor-backend/server.js
+++ money-mentor-backend/server.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import quizRouter from "./routes/quizllm.js";
+import authRouter from "./routes/auth.js";
 
 // Load env variables
 dotenv.config();
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use('/api/quiz', quizRouter); 
+app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
